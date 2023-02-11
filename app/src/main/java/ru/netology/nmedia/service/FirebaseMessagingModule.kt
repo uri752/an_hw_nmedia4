8e47f940c1fe0dd26ef5ab9fc6936bcb4bf6f9ca
uri752 <uri752blag@mail.ru>
2023-02-11 17:24:04 +0300

    ad DI FirebaseMessagingModule

---
+++ app/src/main/java/ru/netology/nmedia/service/FirebaseMessagingModule.kt
@@ -0,0 +1,20 @@
+package ru.netology.nmedia.service
+
+import com.google.firebase.messaging.FirebaseMessaging
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object FirebaseMessagingModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseMessaging(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
+    }
+
+}
